be7ed39bc0c0fd09a282b85cc59f5af7207fdafa
Dan Pellegrini <danpellegrini63@gmail.com>
2025-09-07 19:24:26 -1000

    Initial Commit

---
+++ DPconsult/DPconsultApp.swift
@@ -0,0 +1,17 @@
+//
+//  DPconsultApp.swift
+//  DPconsult
+//
+//  Created by Daniel Pellegrini on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DPconsultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DPconsultTests/DPconsultTests.swift
@@ -0,0 +1,17 @@
+//
+//  DPconsultTests.swift
+//  DPconsultTests
+//
+//  Created by Daniel Pellegrini on 9/7/25.
+//
+
+import Testing
+@testable import DPconsult
+
+struct DPconsultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
